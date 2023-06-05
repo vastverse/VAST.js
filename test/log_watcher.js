20c2abcbd7a0f7c4e41bffa265e0527cbd514b70
Petri Smit <22573216@sun.ac.za>
2023-06-05 11:31:27 +0200

    Added a pause and play command

--- test/log_watcher.js
+++ test/log_watcher.js
@@ -1,10 +1,9 @@
-// Run this file on device with log files to visualise the changes in the Virtual environment
-
 const http = require('http');
 const WebSocket = require('ws');
 const fs = require('fs');
 const express = require('express');
 const path = require('path');
+const readline = require('readline');
 
 const app = express();
 const server = http.createServer(app);
@@ -25,64 +24,86 @@ files.forEach((filePath) => {
   });
 });
 
+let isPaused = false; // Flag to track if input is paused
 
+// Function to handle commands from terminal input
+function handleCommand(command) {
+  if (command === 'pause') {
+    isPaused = true;
+    console.log('Input paused');
+  } else if (command === 'start') {
+    isPaused = false;
+    console.log('Input started');
+  }
+}
+
+// Set up readline interface to listen for terminal input
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+// Listen for terminal input and handle commands
+rl.on('line', (input) => {
+  handleCommand(input.trim());
+});
 
 wss.on('connection', (ws) => {
-    console.log('Client connected');
+  console.log('Client connected');
 
-    // Send the initial file content to the client
-    fs.readFile(filePath, 'utf8', (err, data) => {
+  // Send the initial file content to the client
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error(`Error reading file: ${err}`);
+    } else {
+      ws.send(JSON.stringify({ type: 'fileContent', content: data }));
+    }
+  });
+
+  // Watch the file for changes and send updates to the client
+  const fileWatcher = fs.watch(filePath, (eventType) => {
+    if (eventType === 'change' && !isPaused) {
+      fs.readFile(filePath, 'utf8', (err, data) => {
         if (err) {
-            console.error(`Error reading file: ${err}`);
+          console.error(`Error reading file: ${err}`);
         } else {
-            ws.send(JSON.stringify({ type: 'fileContent', content: data }));
+          ws.send(JSON.stringify({ type: 'fileContent', content: data }));
         }
-    });
+      });
+    }
+  });
 
-    // Watch the file for changes and send updates to the client
-    const fileWatcher = fs.watch(filePath, (eventType) => {
-        if (eventType === 'change') {
-            fs.readFile(filePath, 'utf8', (err, data) => {
-                if (err) {
-                    console.error(`Error reading file: ${err}`);
-                } else {
-                    ws.send(JSON.stringify({ type: 'fileContent', content: data }));
-                }
-            });
-        }
-    });
-
-    ws.on('close', () => {
-        console.log('Client disconnected');
-        fileWatcher.close(); // Stop watching the file when the client disconnects
-    });
+  ws.on('close', () => {
+    console.log('Client disconnected');
+    fileWatcher.close(); // Stop watching the file when the client disconnects
+  });
 });
 
 app.use(express.json());
 
 app.post('/log', (req, res) => {
-    const logData = req.body.logData;
-    const filename = req.body.filename;
-    const directory = req.body.directory;
-    const extension = req.body.extension;
+  const logData = req.body.logData;
+  const filename = req.body.filename;
+  const directory = req.body.directory;
+  const extension = req.body.extension;
 
-    console.log('Received log data:', logData); // Print the received log data
+  console.log('Received log data:', logData); // Print the received log data
 
-    if (!fs.existsSync(directory)) {
-        fs.mkdirSync(directory, { recursive: true });
+  if (!fs.existsSync(directory)) {
+    fs.mkdirSync(directory, { recursive: true });
+  }
+
+  fs.appendFile(path.join(directory, filename + extension), logData, (err) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error writing log data to file');
+    } else {
+      res.status(200).send('Log data written to file');
     }
-
-    fs.appendFile(path.join(directory, filename + extension), logData, (err) => {
-        if (err) {
-            console.error(err);
-            res.status(500).send('Error writing log data to file');
-        } else {
-            res.status(200).send('Log data written to file');
-        }
-    });
+  });
 });
 
 const PORT = process.env.PORT || 1111;
 server.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
+  console.log(`Server started on port ${PORT}`);
 });
